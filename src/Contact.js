39daa0f8722b2edb58847dec73594e0667faa702
123Pankaj1 <kumarpankaj031093@gmail.com>
2023-01-10 22:42:07 +0800

    Initialize contact  application for MFE

---
+++ src/Contact.js
@@ -0,0 +1,15 @@
+const Contact = () => {
+    return (
+        <>
+            <div className="Contact-us">
+                <h1> Contact Us </h1>
+                <img src="https://s3.ap-south-1.amazonaws.com/s3.studytonight.com/tutorials/uploads/pictures/1628677506-101156.png" />
+                <p>ATCS Pvt. Ltd. a Naggaro company</p>
+                <p>Website - www.atcs.com</p>
+                <p>Contact No. - 0141-2345672</p>
+            </div>
+        </>
+    )
+}
+
+export default Contact;
---
+++ src/root.component.js
@@ -0,0 +1,7 @@
+import Contact from "./Contact";
+import './styles/main.css'
+export default function Root(props) {  
+  return (
+    <Contact />
+  );
+}
