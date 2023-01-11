8ebaada9f22471fca62c804c70f1dfce0d88dc0c
123Pankaj1 <kumarpankaj031093@gmail.com>
2023-01-11 17:02:48 +0800

    UX changes

--- src/Contact.js
+++ src/Contact.js
@@ -4,9 +4,7 @@ const Contact = () => {
             <div className="Contact-us">
                 <h1> Contact Us </h1>
                 <img src="https://s3.ap-south-1.amazonaws.com/s3.studytonight.com/tutorials/uploads/pictures/1628677506-101156.png" />
-                <p>ATCS Pvt. Ltd. a Naggaro company</p>
-                <p>Website - www.atcs.com</p>
-                <p>Contact No. - 0141-2345672</p>
+                <p>Demo POC for Micro Front-Ends</p>
             </div>
         </>
     )
